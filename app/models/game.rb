4feb0aefa2dc4ce660536a0031af0be0deb65c63
Andersonvb <andersonvalenciabermude@gmail.com>
2023-03-17 21:09:34 -0500

    Refactor Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -29,55 +29,67 @@ class Game < ApplicationRecord
     create_and_save_couples(couples)
   end
 
+  # Selecciona un trabajador que no haya jugado el año anterior.
   def select_worker_that_will_not_play(workers)
     worker_without_a_pair = workers.sample
 
+     # Verifica si existe un juego anterior
     if Game.exists?(year_game: year_game - 1)
       worker_without_a_pair_last_year = Game.find_by(year_game: year_game - 1).worker_without_a_pair&.worker
 
+       # Si el trabajador elegido jugó en el juego anterior, selecciona otro trabajador
       select_worker_that_will_not_play(workers) if worker_without_a_pair == worker_without_a_pair_last_year
     end
 
     worker_without_a_pair
   end
 
+  # Genera las parejas del juego.
   def generate_couples(workers)
-    couple_combinations = workers.combination(2).to_a
-
-    couples_last_year = couples_to_array(Couple.where(game_id: id - 1).to_a)
-    couples_two_years_ago = couples_to_array(Couple.where(game_id: id - 2).to_a)
+    couple_combinations = generate_possible_couples(workers)
+    couples_last_year = load_couples_from_game(id - 1)
+    couples_two_years_ago = load_couples_from_game(id - 2)
 
     possible_couples = couple_combinations - couples_last_year - couples_two_years_ago
 
     select_couples(possible_couples, workers)
   end
 
+  # Carga las parejas de un juego en especifico.
+  def load_couples_from_game(game_id)
+    couples = Couple.where(game_id: game_id).to_a
+    worker_pairs_from_couples(couples)
+  end
+
+  # Convierte un array de Couples a un array de parejas de Workers.
+  def worker_pairs_from_couples(couples)
+    couples.map { |couple| [couple.first_worker, couple.second_worker] }
+  end
+
+  # Genera todas las posibles parejas de trabajadores.
+  def generate_possible_couples(workers)
+    workers.combination(2).to_a
+  end
+
+  # Selecciona un conjunto de parejas de trabajadores sin repetir trabajadores
   def select_couples(couple_combinations, workers)
     number_of_couples = workers.size / 2
     possible_set_of_couples = couple_combinations.combination(number_of_couples)
 
-    # Seleccionar el conjunto de parejas en el cual no se repita ningun valor.
+    # Obtiene el conjunto de parejas en el cual no se repita ninguno.
     possible_set_of_couples.find do |set_of_couples|
       set_of_couples.flatten.uniq.length == set_of_couples.flatten.length
     end
   end
 
-  def couples_to_array(couples)
-    couple_array = []
-
-    couples.each do |couple|
-      couple_array << [couple.first_worker, couple.second_worker]
-    end
-
-    couple_array
-  end
-
+  # Crea y guarda en la base de datos las parejas.
   def create_and_save_couples(couples)
     couples.each do |first_worker, second_worker|
       Couple.create(game_id: id, first_worker_id: first_worker.id, second_worker_id: second_worker.id)
     end
   end
 
+  # Crea y guarda en la base de datos un trabajador sin pareja.
   def create_and_save_worker_without_a_pair(worker_without_a_pair)
     WorkerWithoutAPair.create(game_id: id, worker_id: worker_without_a_pair.id).save
   end
