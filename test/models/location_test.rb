7a76fe8141c403f1cd4896893e1706e9cce8cdd5
Andersonvb <andersonvalenciabermudez@gmail.com>
2023-04-04 11:32:31 -0500

    Add tests of location model

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -1,7 +1,40 @@
 require "test_helper"
 
 class LocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @location = locations(:location_one)
+  end
+
+  test 'name column' do
+    assert Location.column_names.include?('name') 
+
+    assert_equal 'string', Location.column_for_attribute(:name).type.to_s, 'Correct name type'
+  end
+
+  test 'has_many :workers relation' do
+    worker = workers(:worker_one)
+
+    assert_equal @location, worker.location, 'relation between worker and location'
+  end
+
+  test 'invalid without name' do
+    @location.name = nil
+
+    refute @location.valid?
+    assert @location.errors[:name].present?, 'error without name'
+  end
+  
+  test 'invalid name length' do
+    @location.name = 'Da'
+
+    refute @location.valid?
+    assert @location.errors[:name].present?, 'error with too short name'
+  end
+
+  test 'invalid name with unsafe characters' do
+    @location.name = 'Da*#%n'
+
+    refute @location.valid?
+    assert @location.errors[:name].present?, 'error name with unsafe characters'
+  end
 end
